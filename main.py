62bccf30c5c26167d4db861ee6b2539d129c94f2
houcine1amraoui <houcine1amraoui@gmail.com>
2025-12-12 16:22:28 +0100

    up

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from bst import BST
 bst = BST()
+print(bst.is_valid())
+# assert bst.search_iterative(5) == True
+# assert bst.search_iterative(20) == False
+# assert bst.search_recursive(8) == True
+# assert bst.search_recursive(99) == False
 
-assert bst.search_iterative(5) == True
-assert bst.search_iterative(20) == False
-assert bst.search_recursive(8) == True
-assert bst.search_recursive(99) == False
-
-assert bst.level_order() == [7, 4, 9, 2, 5, 8, 12, 0, 16]
-assert bst.pre_order()   == [7, 4, 2, 0, 5, 9, 8, 12, 16]
-assert bst.in_order()    == [0, 2, 4, 5, 7, 8, 9, 12, 16]
-assert bst.post_order()  == [0, 2, 5, 4, 8, 16, 12, 9, 7]
+# assert bst.level_order() == [7, 4, 9, 2, 5, 8, 12, 0, 16]
+# assert bst.pre_order()   == [7, 4, 2, 0, 5, 9, 8, 12, 16]
+# assert bst.in_order()    == [0, 2, 4, 5, 7, 8, 9, 12, 16]
+# assert bst.post_order()  == [0, 2, 5, 4, 8, 16, 12, 9, 7]
